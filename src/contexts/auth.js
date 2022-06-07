53b233cc00a06401fe686127d568620660b993f1
Alandysson Rolim <alandyssom@gmail.com>
2022-06-07 17:48:22 -0300

    indetify

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -6,81 +6,81 @@ import { format } from 'date-fns';
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    const [carregado, setCarregado] = useState(true);
+   const [user, setUser] = useState(null);
+   const [carregado, setCarregado] = useState(true);
 
-    useEffect(() => {
-        async function carregar(){
-            const dadosUser = await AsyncStorage.getItem('Auth_user');
-            if(dadosUser){
-                setUser(JSON.parse(dadosUser));
-                setCarregado(false);
-            }
+   useEffect(() => {
+      async function carregar() {
+         const dadosUser = await AsyncStorage.getItem('Auth_user');
+         if (dadosUser) {
+            setUser(JSON.parse(dadosUser));
             setCarregado(false);
-        }
-        carregar();
-    }, [])
+         }
+         setCarregado(false);
+      }
+      carregar();
+   }, [])
 
-    async function manter(data){
-        await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
-    }
+   async function manter(data) {
+      await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
+   }
 
 
-    async function adicionar(email , password, nome){
-        if(email.length < 7){
-            return alert(error.code);
-        }
-        await firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(async (value) => {
+   async function adicionar(email, password, nome) {
+      if (email.length < 7) {
+         return alert(error.code);
+      }
+      await firebase.auth().createUserWithEmailAndPassword(email, password)
+         .then(async (value) => {
             let uid = value.user.uid;
 
             await firebase.database().ref('users').child(uid).set({
-                nome: nome,
+               nome: nome,
             })
-            .then(() => {
-                let data = {
-                    uid: uid,
-                    nome: nome, 
-                    email: email
-                };
-                console.log(nome)
-                setUser(data);
-            })
-        })
-    }
+               .then(() => {
+                  let data = {
+                     uid: uid,
+                     nome: nome,
+                     email: email
+                  };
+                  console.log(nome)
+                  setUser(data);
+               })
+         }).catch(error => alert(error.code))
+   }
 
-    async function logar(email, password){
-        await firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(async (value) => {
+   async function logar(email, password) {
+      await firebase.auth().signInWithEmailAndPassword(email, password)
+         .then(async (value) => {
             let uid = value.user.uid
             await firebase.database().ref('users').child(uid).once('value')
-            .then((snapshot) => {
-                let data = {
-                    uid: uid,
-                    nome: snapshot.val().nome,
-                    email: value.user.email,
-                }
-                console.log(data)
-                setUser(data);
-                manter(data);
-            })
-        })
-        .catch((error) => alert(error.code))
+               .then((snapshot) => {
+                  let data = {
+                     uid: uid,
+                     nome: snapshot.val().nome,
+                     email: value.user.email,
+                  }
+                  console.log(data)
+                  setUser(data);
+                  manter(data);
+               })
+         })
+         .catch((error) => alert(error.code))
 
-    }
+   }
 
-    async function deslogar(){
-        await firebase.auth().signOut()
-        await AsyncStorage.clear()
-        .then(() => {
+   async function deslogar() {
+      await firebase.auth().signOut()
+      await AsyncStorage.clear()
+         .then(() => {
             setUser(null)
-        })
-    }
-    return (
-    <AuthContext.Provider value={{logado: !!user, user, carregado, adicionar, logar, deslogar}}>
-        {children}
-    </AuthContext.Provider>
-        );
+         })
+   }
+   return (
+      <AuthContext.Provider value={{ logado: !!user, user, carregado, adicionar, logar, deslogar }}>
+         {children}
+      </AuthContext.Provider>
+   );
 }
 
 export default AuthProvider;
